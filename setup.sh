8c5cc3bd3600815adabc0e264dcf739fd29bf4dc
Samuel Lowe <samlowe106@gmail.com>
2025-01-12 20:13:17 -0500

    Numpy will now be installed after tensorflow to avoid versioning issues

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ pipx ensurepath
 pipx install poetry
 
 poetry init
-poetry add matplotlib notebook numpy pandas scikit-learn seaborn tensorflow torch
+poetry add matplotlib notebook pandas scikit-learn seaborn tensorflow torch
+poetry add numpy # tensorflow etc require a lower version of numpy than current release
 poetry install
