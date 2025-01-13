b6ba0fa12c55beb3eb826c18e3e92bf54f7a0666
Samuel Lowe <samlowe106@gmail.com>
2025-01-12 20:18:43 -0500

    Can't add numpy as dependency

--- setup.sh
+++ setup.sh
@@ -5,5 +5,4 @@ pipx install poetry
 
 poetry init
 poetry add matplotlib notebook pandas scikit-learn seaborn tensorflow torch
-poetry add numpy # tensorflow etc require a lower version of numpy than current release
 poetry install
