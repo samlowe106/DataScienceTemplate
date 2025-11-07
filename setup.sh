aa2751283b449c2d1115a5310952290b3b8e4ceb
Samuel Lowe <samlowe106@gmail.com>
2025-11-07 00:19:53 -0500

    Changed dependencies, added pre-commit, and used pyenv instead

--- setup.sh
+++ setup.sh
@@ -1,14 +1,22 @@
-python -m venv .venv
-source venv/bin/activate
+#python -m venv .venv
+#source venv/bin/activate
+pyenv local 3.14
 
 #apt install pipx -y
 #pipx ensurepath
 #pipx install poetry
 
 poetry init
+# only installing top level dependencies -- numpy, pandas, matplotlib are included
 # Additional dependencies should be added below!
-poetry add matplotlib notebook pandas scikit-learn seaborn tensorflow tensorflow-datasets torch
+
+poetry add notebook geopandas geopy scikit-learn seaborn tensorflow tensorflow-datasets torch pre-commit
 poetry install
 
-mkdir data
+pre-commit autoupdate
+pre-commit install
+
+mkdir data/raw
+mkdir data/clean
+
 rm setup.sh
