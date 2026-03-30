9d5d7647dd1fcaea538beae20bf85746c17a58e4
Samuel Lowe <samlowe106@gmail.com>
2026-03-30 17:28:11 -0400

    Removed unnecessary mkdir commands

--- setup.sh
+++ setup.sh
@@ -3,13 +3,9 @@ uv init
 
 # only installing top level dependencies -- numpy, pandas, matplotlib are included
 # Additional dependencies should be added below!
-uv add notebook geopandas geopy scikit-learn seaborn tensorflow tensorflow-datasets torch pre-commit
+uv add httpx notebook geopandas geopy scikit-learn seaborn tensorflow tensorflow-datasets torch pre-commit
 
 pre-commit autoupdate
 pre-commit install
-
-mkdir data
-mkdir data/raw
-mkdir data/clean
 
 rm setup.sh
